a50c24570aa76aa80d74199f3fb7556adfbecd12
Teddy (Gabby) <113390941+Appl3Kitti3@users.noreply.github.com>
2024-09-29 15:27:47 -0700

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,9 +1,13 @@
+// Utils class, contains static utility functions
 class Utils
 {
+
+    // returns server's date
     static getDate()
     {
         return Date();
     }
 }
 
+// export the class
 module.exports = Utils.getDate;
