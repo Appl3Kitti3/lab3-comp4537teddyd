f827caba687bfdd2f37060db83cfd47a8ae0c097
Teddy (Gabby) <113390941+Appl3Kitti3@users.noreply.github.com>
2024-09-29 15:28:05 -0700

    Update en.js

--- lang/en/en.js
+++ lang/en/en.js
@@ -1,6 +1,7 @@
-const HELLO = "Hello";
-const BEAUTIFUL_DAY = ", What a beautiful day. Server current date and time is *";
-const HEADER_OPENING = "<h1 style='color: blue'>";
-const HEADER_CLOSING = "</h1>";
+const HELLO = "Hello"; // This is a simple string for hello
+const BEAUTIFUL_DAY = ", What a beautiful day. Server current date and time is *"; // Beautiful Day message
+const HEADER_OPENING = "<h1 style='color: blue'>"; // Opening tag for header 1 with blue color
+const HEADER_CLOSING = "</h1>"; // Closing tag for header 1
 
+// export everything
 module.exports = { HELLO, BEAUTIFUL_DAY, HEADER_OPENING, HEADER_CLOSING };
